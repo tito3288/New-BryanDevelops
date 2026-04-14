a71a57f4b0489f139ed970601070345e39582232
Bryan Arambula <arambula722@gmail.com>
2026-04-14 12:57:04 -0400

    redirect set for for old pages to redirect to Web Development page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/website-design-development",
+        destination: "/services/web-development",
+        permanent: true,
+      },
+      {
+        source: "/website-design-development/",
+        destination: "/services/web-development",
+        permanent: true,
+      },
+      {
+        source: "/best-website-builders",
+        destination: "/services/web-development",
+        permanent: true,
+      },
+      {
+        source: "/best-website-builders/",
+        destination: "/services/web-development",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
